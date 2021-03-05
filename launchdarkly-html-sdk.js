dee2b272b44bfbf8ace93c0a1b323a9bfb7ad932
Yoz Grahame <ygrahame@launchdarkly.com>
2021-03-05 11:20:54 -0800

    Initial SDK structure

---
+++ launchdarkly-html-sdk.js
@@ -0,0 +1,28 @@
+(function(){
+
+    
+    var activeElements = getActiveElements();
+    var client_key = getClientKey();
+    var ldClient = loadLdJsSDK(initSdk);
+
+    // Returns an array of all elements with ld-data-* attributes
+    function getActiveElements() {
+
+    }
+
+    // Returns the client key specified in a data-ld-clientkey attr
+    function getClientKey() {
+
+    }
+
+    // Dynamically loads the LaunchDarkly browser-JS SDK
+    function loadLdJsSDK(){
+
+    }
+
+    // Performs initial flag evaluations and sets up change handlers
+    function initSdk() {
+        
+    }
+
+}())
